03856c468027bf49528d8abd7c2254c1c8b2f0e2
Onlysuper <2483613372@qq.com>
2020-01-21 18:02:53 +0800

    主页面初始化

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+import { Button, Table,Layout,Header,Footer,Menu,MenuItem,Sider,Icon } from 'view-design';
+Vue.component('Button', Button);
+Vue.component('Table', Table);
+Vue.component('Layout', Layout);
+Vue.component('Header', Header);
+Vue.component('Footer', Footer);
+Vue.component('Menu', Menu);
+Vue.component('MenuItem', MenuItem);
+Vue.component('Sider', Sider);
+Vue.component('Icon', Icon);
 
 Vue.config.productionTip = false;
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Layout from '../views/Layout.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'layout',
+    component: Layout,
   },
   {
     path: '/about',
